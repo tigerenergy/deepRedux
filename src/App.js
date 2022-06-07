88ec699ce5e3dfae426520f6001d40cf5c3e7f95
tigerenergy <kisoon0947@gmail.com>
2022-06-07 16:36:30 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import { useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 import Cart from './components/Cart/Cart'
 import Layout from './components/Layout/Layout'
 import Products from './components/Shop/Products'
+import { uiActions } from './store/ui-slice'
+import Notification from './components/UI/Notification'
+
+let isInitial = true
 
 function App() {
+  const dispatch = useDispatch()
   const showCart = useSelector((state) => state.ui.cartIsVisible)
   const cart = useSelector((state) => state.cart)
+  const notification = useSelector((state) => state.ui.notification)
 
   useEffect(() => {
     const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
+      )
       const response = await fetch('https://react-http-a3d8c-default-rtdb.firebaseio.com/cart.json', {
         method: 'PUT',
         body: JSON.stringify(cart),
@@ -18,11 +31,33 @@ function App() {
       if (!response.ok) {
         throw new Error('Sending cart data failed.')
       }
-      const responseData = response.json()
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        })
+      )
     }
-  }, [cart])
+
+    if (isInitial) {
+      isInitial = false
+      return
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
+        })
+      )
+    })
+  }, [cart, dispatch])
   return (
     <>
+      {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
       <Layout>
         {showCart && <Cart />}
         <Products />
